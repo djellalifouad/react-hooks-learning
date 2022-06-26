f6e3c0bb3f4cafcac92ca568217f575682cf0a38
fouadesi <83358349+djellalifouad@users.noreply.github.com>
2022-06-26 14:17:06 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import React , {useState , useEffect} from 'react'; 
+import React , {useState , useEffect,useRef} from 'react'; 
 function App() {
-  //const [count , setCount] = useState(0);
-  const [count,setCount] = useState(0);
-  const [title, setTitle]= useState('title'); 
-  const handleCount = ()=> setCount((c)=> c+1); 
-  const handleTitle= ()=> setTitle('algorithm academy')
-    useEffect(() => {
-      console.log("use Effect1");
-    }, []);
-  useEffect(()=> {
-    console.log('use count')
-  },[count])
-   useEffect(() => {
-     console.log("use title");
-   },[title]);
+  const mail = useRef(); 
+  const password= useRef(); 
+  const handleSubmit = (e) => {
+    console.log('mail',mail.current.value)
+    console.log('password',password.current.value);
+  }
   return (
     <div className="App">
-      <button onClick={handleCount}>{count}</button>
-      <button onClick={handleTitle}>{title}</button>
+      <input type="text"  ref={mail} />
+      <input type="text" ref={password} />
+      <button onClick={handleSubmit}>test</button>
     </div>
   );
 }
