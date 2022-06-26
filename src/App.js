40502760c5ee7da587b09372e34f4dbc7975f0b9
fouadesi <83358349+djellalifouad@users.noreply.github.com>
2022-06-26 13:15:18 +0100

    useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React , {useState} from 'react'; 
+import React , {useState , useEffect} from 'react'; 
 function App() {
   //const [count , setCount] = useState(0);
-  const [{count,count2},setCount] = useState({count:0,count2:1});
+  const [count,setCount] = useState(0);
+  const [title, setTitle]= useState('title'); 
+  const handleCount = ()=> setCount((c)=> c+1); 
+  const handleTitle= ()=> setTitle('algorithm academy')
+    useEffect(() => {
+      console.log("use Effect1");
+    }, []);
+  useEffect(()=> {
+    console.log('use count')
+  },[count])
+   useEffect(() => {
+     console.log("use title");
+   },[title]);
   return (
     <div className="App">
-    <p onClick={() => setCount(currentState => ({...currentState,count:currentState.count+1}))}> you clicked {count}</p>
+      <button onClick={handleCount}>{count}</button>
+      <button onClick={handleTitle}>{title}</button>
     </div>
   );
 }
